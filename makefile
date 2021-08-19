19f12342a909379657b31ed9997d90783e6c28c4
Gustavo Carvalho <gustavo1net@hotmail.com>
2021-08-19 20:19:34 -0300

    ft_isalpha and test and makefile

---
+++ makefile
@@ -0,0 +1,17 @@
+COMPILER = gcc
+TAGS = -Wall -Wextra -Werror
+
+MAIN = ./main.c
+TESTS = ./tests/*.c
+FUNCTIONS = ./functions/*.c
+
+PNAME = ./main.out
+
+all: compiler exe
+sall: scompiler exe
+compiler:
+	${COMPILER} ${TAGS} ${MAIN} ${TESTS} ${FUNCTIONS} -o ${PNAME}
+scompiler:
+	${COMPILER} ${MAIN} ${TESTS} ${FUNCTIONS} -o ${PNAME}
+exe:
+	${PNAME}
---
+++ tests/t_isalpha.c
@@ -0,0 +1,44 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   t_isalpha.c                                        :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: glima-de <glima-de@student.42sp.org.br>    +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2021/08/19 20:04:57 by glima-de          #+#    #+#             */
+/*   Updated: 2021/08/19 20:04:57 by glima-de         ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#include <ctype.h>
+#include <stdlib.h>
+#include <unistd.h>
+#include <stdio.h>
+int	ft_isalpha(int c);
+
+void	applyTest(void)
+{
+	int		i;
+	int		r_ori;
+	int		r_cus;
+
+	i = -255;
+	while (i < 255)
+	{
+		r_ori = isalpha(i);
+		r_cus = ft_isalpha(i);
+		if (r_ori != r_cus)
+		{
+			printf("KO! sended %d and expected %d, got %d", i, r_ori, r_cus);
+			break ;
+		}
+		i++;
+	}
+	if (i == 255)
+		printf("ft_isalpha: OK!");
+}
+
+void	test_isalpha(void)
+{
+	applyTest();
+}
