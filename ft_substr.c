c3305172bcff23fc744b94be11abb30cb35f7c3d
Gustavo Carvalho <gustavo1net@hotmail.com>
2021-09-04 14:42:50 -0300

    count size len in string

--- ft_substr.c
+++ ft_substr.c
@@ -6,33 +6,39 @@
 /*   By: glima-de <glima-de@student.42sp.org.br>    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2021/08/24 18:31:11 by glima-de          #+#    #+#             */
-/*   Updated: 2021/09/04 13:13:46 by glima-de         ###   ########.fr       */
+/*   Updated: 2021/09/04 14:42:05 by glima-de         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "libft.h"
 
+#include <stdlib.h>
+#include <unistd.h>
+#include <stdio.h>
+
+unsigned int	ft_strlen(const char *str);
+
 char	*ft_substr(char const *s, unsigned int start, size_t len)
 {
 	unsigned int	i;
+	unsigned int	rlen;
 	char			*aux;
 
 	i = 0;
-	if (start + len > ft_strlen(s))
-		len = len - (ft_strlen(s) - (start + len));
-	aux = malloc((len + 1) * sizeof(char));
+	rlen = len;
+	if (start + rlen > ft_strlen(s))
+		rlen = ft_strlen(s) - (start + rlen);
+	if ((int)rlen < 0)
+		rlen = 0;
+	aux = malloc((rlen + 1) * sizeof(char));
 	if (aux)
 	{
-		while (i < len)
+		while (i < rlen)
 		{
 			aux[i] = s[i + start];
 			i++;
 		}
-		while (i <= len)
-		{
-			aux[i] = '\0';
-			i++;
-		}
+		aux[i] = '\0';
 		return (aux);
 	}
 	else
